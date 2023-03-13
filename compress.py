f07df309b0588e7ff36453638314f3d393341bb2
m-michurov <mikhail.michurov.00@gmail.com>
2023-03-13 14:24:13 +0700

    Quantize image to 8bpp using color clusters as palette

--- compress.py
+++ compress.py
@@ -7,43 +7,77 @@ import numpy as np
 from sklearn.cluster import MiniBatchKMeans
 
 
+def make_palette(color_space: np.ndarray) -> list[int]:
+    color_space = np.clip(color_space, 0, 255).astype('uint8')
+
+    colors, channels = color_space.shape
+    palette_data = list(color_space.reshape((channels * colors,)))
+
+    palette = (palette_data * (256 // colors))
+
+    return palette
+
+
+def compress(
+        image: Any,
+        target_bpp: int,
+        random_state: int | None = None
+) -> Image.Image:
+    assert 0 < target_bpp <= 8
+
+    pixels: np.ndarray = np.array(image)
+
+    height, width, channels = pixels.shape
+    pixels = pixels.reshape((height * width, channels))
+
+    # noinspection PyArgumentEqualDefault
+    kmeans = MiniBatchKMeans(
+        init='k-means++',
+        n_clusters=2**target_bpp,
+        random_state=random_state
+    )
+    kmeans.fit(pixels)
+
+    pixels = kmeans.labels_.astype('uint8')
+    result = Image.fromarray(pixels.reshape((height, width)), mode='P')
+    result.putpalette(make_palette(color_space=kmeans.cluster_centers_))
+
+    return result
+
+
 @click.command()
 @click.argument('image_path', type=Path)
-@click.argument('n_colors', type=int)
+@click.option(
+    '--bpp',
+    required=True,
+    type=int,
+    help='Must be in range [1, 8]'
+)
 @click.option(
     '--out',
     required=False,
     type=Path
 )
+@click.option(
+    '--random-state',
+    required=False,
+    type=int
+)
 def main(
         image_path: Path,
-        n_colors: int,
-        out: Path | None = None
+        bpp: int,
+        out: Path | None = None,
+        random_state: int | None = None
 ) -> None:
-    assert n_colors > 0
-
     if out is None:
-        out = Path(f'{image_path.stem}-{n_colors}{image_path.suffix}')
+        out = Path(f'{image_path.stem}-{2 ** bpp}{image_path.suffix}')
 
-    pil_image: Any = Image.open(image_path).convert('RGB')
-    image: np.ndarray = np.array(pil_image)
-
-    height, width, channels = image.shape
-    image = image.reshape(height * width, channels)
-
-    # noinspection PyArgumentEqualDefault
-    kmeans = MiniBatchKMeans(
-        init='k-means++',
-        n_clusters=n_colors
-    )
-    kmeans.fit(image)
-
-    # Pixel -> Centroid
-    compressed_image = kmeans.cluster_centers_[kmeans.labels_]
-    compressed_image = np.clip(compressed_image.astype('uint8'), 0, 255)
-    compressed_image = compressed_image.reshape((height, width, channels))
-
-    Image.fromarray(compressed_image).save(out)
+    with Image.open(image_path).convert('RGB') as source_image:
+        with compress(source_image, bpp, random_state) as compressed_image:
+            try:
+                compressed_image.save(out)
+            except OSError:
+                compressed_image.convert('RGB').save(out)
 
 
 if __name__ == '__main__':
